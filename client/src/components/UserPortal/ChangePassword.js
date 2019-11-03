5f4155a5fb298fe95596cff8528ba8458b30a82a
Connor Wolfe <Connor@Connors-MacBook-Pro-2.local>
2019-11-03 15:33:04 -0500

    Used Davids code to make a change password section.

--- client/src/components/UserPortal/ChangePassword.js
+++ client/src/components/UserPortal/ChangePassword.js
@@ -1,14 +1,79 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const ChangePassword = () => {
+class ChangePassword extends Component {
+    constructor(props) {
+        super(props);
 
-    return (
-        <div>
-            <p>
-                Linked Password Component
-            </p>
-        </div>
-    )
+        this.state = {
+            newpass: '',
+            newpassconfirm: '',
+            passwordFieldError: '',
+        };
+    };
+
+    handlePasswordChange = event => {
+        this.setState({
+            newpass: event.target.value
+        }, () => {
+            this.passwordValidation();
+        })
+    };
+
+    handlePasswordChangeConfirm = event => {
+        this.setState({
+            newpassconfirm: event.target.value
+        }, () => {
+            this.passwordValidation();
+        })
+    };
+
+    passwordValidation = () => {
+        const password1 = this.state.newpass;
+        const password2 = this.state.newpassconfirm;
+        this.setState({
+                passwordFieldError: password1 === password2 ? null :
+                    'The passwords do not match.'
+            }
+        )
+    };
+
+    handleSubmit = event => {
+        event.preventDefault();
+    };
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <div className="form-group">
+                    <label htmlFor="password">Enter your new password</label>
+                    <input name="name"
+                           className={`form-control ${this.state.passwordFieldError ? 'is-invalid' : ''}`}
+                           id="password"
+                           placeholder="Enter your new password"
+                           value={this.state.newpass}
+                           onChange={this.handlePasswordChange}
+                           onBlur={this.passwordValidation}
+                    />
+                    <div className='invalid-feedback'></div>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password2">Confirm your new password</label>
+                    <input name="password2"
+                           className={`form-control ${this.state.passwordFieldError ? 'is-invalid' : ''}`}
+                           id="password2"
+                           placeholder="Enter your new password again"
+                           value={this.state.newpassconfirm}
+                           onChange={this.handlePasswordChangeConfirm}
+                           onBlur={this.passwordValidation}
+                    />
+                    <div className='invalid-feedback'>{this.state.passwordFieldError}</div>
+                </div>
+                <button type="submit"
+                        className="btn btn-success btn-block">Submit
+                </button>
+            </form>
+        )
+    }
 }
 
 export default ChangePassword;
