d867814e40d438cad21fb351da323b7e76ae09e1
dmeadows <meadows.david@siemens.com>
2019-11-04 16:53:44 -0500

    added the useUnifiedTopology option to avoid deprecation warning.
    added a emailPreferences model file and model test file. Needs additional work.

---
+++ server/models/emailPreferences.server.model.js
@@ -0,0 +1,35 @@
+//You can replace this entire file with your Bootcamp Assignment #2 - ListingSchema.js File
+
+/* Import mongoose and define any variables needed to create the schema */
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+/* Create your schema */
+var emailSchema = new Schema({
+        emailAddress: {type: String, required: true},
+    },
+    {
+        timestamps: true
+    });
+
+/* create a 'pre' function that adds the updated_at and created_at if not already there property */
+emailSchema.pre('save', function (next) {
+    // get the current date
+
+    var currentDate = new Date();
+    // change the updated_at field to current date
+    this.updated_at = currentDate;
+
+    // if created_at doesn't exist, add to that field
+    if (!this.created_at) {
+        this.created_at = currentDate;
+    }
+
+    next();
+});
+
+/* Use your schema to instantiate a Mongoose model */
+var EmailPreference = mongoose.model('EmailPreference', emailSchema);
+
+/* Export the model to make it avaiable to other parts of your Node application */
+module.exports = EmailPreference;
