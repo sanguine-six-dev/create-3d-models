5fa6cb91cc4929d77a9a52efb8e6f4168428f980
Connor Wolfe <Connor@Connors-MacBook-Pro-2.local>
2019-11-12 16:18:25 -0500

    Added the user login and register landing page to through app.js, header.js, and created a new folder for the components within. It is called UserLogin

---
+++ client/src/components/UserLogin/Login.js
@@ -0,0 +1,87 @@
+import React, {Component} from 'react'
+
+class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: '',
+            userFieldError: '',
+            passwordFieldError: ''
+        };
+    };
+
+    handleUserChange = e => {
+        this.setState({
+            username:e.target.value
+        });
+    };
+
+    handlePassChange = e => {
+        this.setState({
+            password:e.target.value
+        });
+    };
+
+    handleLogin = event => {
+        event.preventDefault();
+    };
+
+    usernameValidation = () => {
+        const username = this.state.username;
+        this.setState({
+                userFieldError: (username.length > 1 && username.includes("@") && username.includes(".")) ? null :
+                    'Please enter a valid email address'
+            }
+        )
+    };
+
+    //This can be changed later to meet whatever password requirements are necessary
+    //Just add them to the ternary statement
+    passwordValidation = () => {
+        const password = this.state.password;
+        this.setState({
+                passwordFieldError: (password.length > 5) ? null :
+                    'Please enter a password with more than 5 characters'
+            }
+        )
+    };
+
+    render() {
+        return(
+            <div id="myLoginBox">
+                <form className="justify-content-center" onSubmit={this.handleLogin}>
+                    <div className="form-group">
+                        <label htmlFor="Login">Username</label>
+                        <input
+                            value = {this.state.username}
+                            className={`form-control ${this.state.userFieldError ? 'is-invalid' : ''}`}
+                            placeholder="Enter your username"
+                            onChange={this.handleUserChange}
+                            onBlur={this.usernameValidation}
+                        />
+                        <div className='text-danger'>{this.state.userFieldError}</div>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="Password">Password</label>
+                        <input
+                            type="password"
+                            value = {this.state.password}
+                            className={`form-control ${this.state.passwordFieldError ? 'is-invalid' : ''}`}
+                            placeholder="Enter your password"
+                            onChange={this.handlePassChange}
+                            onBlur={this.passwordValidation}
+                        />
+                        <div className='text-danger'>{this.state.passwordFieldError}</div>
+                    </div>
+                    <button type="submit"
+                            className="btn btn-primary btn-block">Login
+                    </button>
+                </form>
+            </div>
+        )
+    }
+
+}
+
+export default Login
---
+++ client/src/components/UserLogin/Register.js
@@ -0,0 +1,186 @@
+import React, {Component} from 'react'
+
+
+class Register extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            first_name: '',
+            last_name: '',
+            email_address: '',
+            password: '',
+            passwordconfirm: '',
+            phone_number: '',
+            address: '',
+            emailFieldError: '',
+            firstFieldError: '',
+            lastFieldError: '',
+            passFieldError: '',
+            pass2FieldError: ''
+        };
+    };
+
+    handleRegister = event => {
+        event.preventDefault();
+    };
+
+    handleFirstChange = e => {
+        this.setState({
+            first_name:e.target.value
+        });
+    };
+
+    handleLastChange = e => {
+        this.setState({
+            last_name:e.target.value
+        });
+    };
+
+    handleEmailChange = e => {
+        this.setState({
+            email_address:e.target.value
+        });
+    };
+
+    handlePasswordChange = e => {
+        this.setState({
+            password:e.target.value
+        });
+    };
+
+    handlePassword2Change = e => {
+        this.setState({
+            passwordconfirm:e.target.value
+        });
+    };
+
+    emailValidation = () => {
+        const email = this.state.email_address;
+        this.setState({
+                emailFieldError: (email.length > 1 && email.includes("@") && email.includes(".")) ? null :
+                    'Please enter a valid email address'
+            }
+        )
+    };
+
+    firstValidation = () => {
+        const first = this.state.first_name;
+        this.setState({
+                firstFieldError: (first.length > 0) ? null :
+                    'Please enter a first name'
+            }
+        )
+    };
+
+    lastValidation = () => {
+        const last = this.state.last_name;
+        this.setState({
+                lastFieldError: (last.length > 0) ? null :
+                    'Please enter a last name'
+            }
+        )
+    };
+
+    passwordValidation = () => {
+        const password = this.state.password;
+        this.setState({
+                passFieldError: (password.length >= 5) ? null :
+                    'Please enter a password with more than 5 characters.'
+            }
+        )
+    };
+
+    password2Validation = () => {
+        const password = this.state.password
+        const password2 = this.state.passwordconfirm;
+        this.setState({
+                pass2FieldError: (password === password2) ? null :
+                    'Passwords must match'
+            }
+        )
+    };
+
+    render() {
+        console.log(this.state);
+        return(
+            <div>
+                <form className="justify-content-center" onSubmit={this.handleRegister}>
+                    <div class="form-row d-flex justify-content-around">
+                        <div class="form-group col-md-6">
+                            <label htmlFor="First name">First Name</label>
+                            <input
+                                name="first_name"
+                                type="text"
+                                placeholder="First"
+                                value={this.state.first_name}
+                                onChange={this.handleFirstChange}
+                                onBlur={this.firstValidation}
+                                class="form-control"
+                            />
+                            <div className='text-danger'>{this.state.firstFieldError}</div>
+                        </div>
+                        <div className="form-group col-md-6">
+                            <label htmlFor="Last name">Last name</label>
+                            <input
+                                name="last_name"
+                                type="text"
+                                placeholder="Last"
+                                value={this.state.last_name}
+                                onChange={this.handleLastChange}
+                                onBlur={this.lastValidation}
+                                class="form-control"
+                            />
+                            <div className='text-danger'>{this.state.lastFieldError}</div>
+                        </div>
+                    </div>
+                    <div class="form-row d-flex justify-content-center">
+                        <div className="form-group col-md-12">
+                            <label>Email Address</label>
+                            <input
+                                name="email_address"
+                                placeholder="example@example.com"
+                                type="email"
+                                value={this.state.email_address}
+                                onChange={this.handleEmailChange}
+                                onBlur={this.emailValidation}
+                                class="form-control"
+                            />
+                            <div className='text-danger'>{this.state.emailFieldError}</div>
+                        </div>
+                    </div>
+                    <div className="form-row d-flex justify-content-around">
+                        <div className="form-group col-md-6">
+                            <label htmlFor="Password">Enter your password</label>
+                            <input
+                                name="password"
+                                type="password"
+                                value={this.state.password}
+                                onChange={this.handlePasswordChange}
+                                className="form-control"
+                                onBlur={this.passwordValidation}
+                            />
+                            <div className='text-danger'>{this.state.passFieldError}</div>
+                        </div>
+                        <div className="form-group col-md-6">
+                            <label htmlFor="Password2">Confirm your password</label>
+                            <input
+                                name="passwordconfirm"
+                                type="password"
+                                value={this.state.passwordconfirm}
+                                onChange={this.handlePassword2Change}
+                                className="form-control"
+                                onBlur={this.password2Validation}
+                            />
+                            <div className='text-danger'>{this.state.pass2FieldError}</div>
+                        </div>
+                    </div>
+                    <button type="submit"
+                            className="btn btn-primary btn-block">Register
+                    </button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Register
